7c0b7ef69aa051ded250c694ad529fff8a879c7a
Mateus Weiss <mateuswmedeiros@gmail.com>
2026-03-19 15:34:54 -0300

    Adicionada a verificação de numero

---
+++ Ex 03/Verificação-de-numero.py
@@ -0,0 +1,7 @@
+number = int(input("Digite um número: "))
+if number >= 10 and number <= 50:
+    print("O número está entre 10 e 50.")
+elif number < 10:
+    print("O número é menor que 10.")
+elif number > 50:   
+    print("O número é maior que 50.")
