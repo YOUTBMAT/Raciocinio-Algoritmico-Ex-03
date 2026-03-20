9c34323587d28a79e690a7ca5da5b142502f76ae
Mateus Weiss Medeiros <mateuswmedeiros@gmail.com>
2026-03-20 10:56:37 -0300

    Correção de print

--- Ex 03/Ano-bissexto.py
+++ Ex 03/Ano-bissexto.py
@@ -2,8 +2,8 @@ ano = input("Digite um ano para verificar se é bissexto: ")
 if ano.isdigit():
     ano = int(ano)
     if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0):
-        print(f"O ano {ano} é bissexto.")
+        print(f"O ano {ano} é bissexto")
     else:
-        print(f"O ano {ano} não é bissexto.")
+        print(f"O ano {ano} não é bissexto")
 else:
-    print("Por favor, digite um ano válido.")
+    print("Por favor, digite um ano válido")
